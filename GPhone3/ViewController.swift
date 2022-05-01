a6d3ed8bdde701cbebaa4c0d2ec9b8be19f95197
Red <redtseng@hahow.in>
2022-05-01 23:11:33 +0800

    Initial Commit

---
+++ GPhone3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GPhone3
+//
+//  Created by Red on 2022/5/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
